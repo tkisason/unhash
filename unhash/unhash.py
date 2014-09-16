c5b926054e7578d092f53ae30204a244f4308603
Igor Vuk <parcijala@gmail.com>
2014-09-16 20:18:21 +0200

    PEP 8 fixes for unhash.py

--- unhash/unhash.py
+++ unhash/unhash.py
@@ -8,14 +8,16 @@ import itertools
 import signal
 import multiprocessing
 
+
 # If you try to write fast python code, you will have a baaaad time.
 
-def generator(file,repl = {},upper = [],rperm=0,sfalse=0, s=(None,None)):
+
+def generator(file, repl={}, upper=[], rperm=0, sfalse=0, s=(None, None)):
     ## Yes, this function is ugly, but we are trying to reduce function call overheads in python interpreter
     ## Each function call adds additional overhead into already slow performance of the interpreter.
     if 'ALL' in upper:
-        upper = range(0,100)
-    if len(upper) > 0 and len(repl) == 0: ## Only uppercase some words
+        upper = range(0, 100)
+    if len(upper) > 0 and len(repl) == 0:  # Only uppercase some words
         with open(file) as wordlist:
             for line in wordlist:
                 out = list(line.rstrip())
@@ -25,12 +27,12 @@ def generator(file,repl = {},upper = [],rperm=0,sfalse=0, s=(None,None)):
                 except:
                     pass
                 yield ''.join(out)[slice(*s)]
-    elif len(upper) == 0 and len(repl) > 0 and rperm == 0 and sfalse == 0: # fast REPL
+    elif len(upper) == 0 and len(repl) > 0 and rperm == 0 and sfalse == 0:  # fast REPL
         with open(file) as wordlist:
             for line in wordlist:
                 out = line
                 for el in repl:
-                    out = out.replace(el,repl[el]).rstrip()
+                    out = out.replace(el, repl[el]).rstrip()
                 yield out[slice(*s)]
     else:
         with open(file) as wordlist:
@@ -39,7 +41,7 @@ def generator(file,repl = {},upper = [],rperm=0,sfalse=0, s=(None,None)):
                     mlen = 0
                     for re in repl:
                         mlen += line.count(re)
-                    for mask in itertools.product('01',repeat=mlen):
+                    for mask in itertools.product('01', repeat=mlen):
                         mid = 0
                         pl = list(line)
                         for el in range(len(pl)):
@@ -74,22 +76,26 @@ def file_to_dictlist(file):
     dl = []
     with open(file) as a:
         for line in a.readlines():
-            dl.append( eval(line.rstrip()))
+            dl.append(eval(line.rstrip()))
     return dl
+
 
 def stripped_openfile(handle):
     with open(handle) as file:
         for line in file:
             yield line.rstrip()
 
-def bfgenerator(generate,length):
-    for el in itertools.product(generate,repeat=length):
+
+def bfgenerator(generate, length):
+    for el in itertools.product(generate, repeat=length):
             yield ''.join(el)
 
-def bf_L_generator(generate,tolength):
-    for l in range(1,tolength+1):
-        for el in itertools.product(generate,repeat=l):
+
+def bf_L_generator(generate, tolength):
+    for l in range(1, tolength+1):
+        for el in itertools.product(generate, repeat=l):
                 yield ''.join(el)
+
 
 def specify_generator(elem):
     if type(elem) == str:
@@ -101,11 +107,11 @@ def specify_generator(elem):
         rperm = 0
         sfalse = 0
         cf = ct = None
-        s = (None,None)
+        s = (None, None)
         if elem.has_key('g') and len(elem) == 2:
-            return bfgenerator(elem['g'],elem['l'])
+            return bfgenerator(elem['g'], elem['l'])
         if elem.has_key('gl') and len(elem) == 2:
-            return bf_L_generator(elem['gl'],elem['l'])
+            return bf_L_generator(elem['gl'], elem['l'])
         if elem.has_key('f') and len(elem) == 1:
             return stripped_openfile(elem['f'])
         if elem.has_key('f'):
@@ -124,7 +130,8 @@ def specify_generator(elem):
             sfalse = 1
         if elem.has_key('s'):
             s = elem['s']
-        return generator(file,repl,upper,rperm,sfalse,s)
+        return generator(file, repl, upper, rperm, sfalse, s)
+
 
 def p(*args):
     gdef = []
@@ -135,6 +142,7 @@ def p(*args):
         #sys.stdout.write(''.join(map(lambda x: x.rstrip(),el))+'\n')
         sys.stdout.write(''.join(el)+'\n')
 
+
 def gen(argspec):
     gdef = []
     sys.stderr.write('[i] running rule: ' + str(argspec) + '\n')
@@ -144,19 +152,22 @@ def gen(argspec):
         #sys.stdout.write(''.join(map(lambda x: x.rstrip(),el))+'\n')
         sys.stdout.write(''.join(el)+'\n')
 
+
 def initw():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-def crack(pattern,NUMCPUS=2):
+
+def crack(pattern, NUMCPUS=2):
     i = 0
-    pool = multiprocessing.Pool(processes=NUMCPUS,initializer=initw)
-    pool.map(gen,pattern)
+    pool = multiprocessing.Pool(processes=NUMCPUS, initializer=initw)
+    pool.map(gen, pattern)
     pool.close()
     pool.join()
 
+
 if __name__ == '__main__':
     import argparse
-    desc ='''
+    desc = '''
     UNHash - rule based password cracker written for pypy\n
     Tonimir Kišasondi (c) 2012 -\n
     protip: SMP support in pypy cannot be interrupted with CTRL+C, use:\n
@@ -165,7 +176,7 @@ if __name__ == '__main__':
     '''
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('RULE_FILE', help='The rulefile you want to run')
-    args=parser.parse_args()
+    args = parser.parse_args()
     u = 'u'
     f = 'f'
     r = 'r'
@@ -176,4 +187,4 @@ if __name__ == '__main__':
     l = 'l'
     s = 's'
     execfile(args.RULE_FILE)
-    crack(RULES,NUMCPUS)
+    crack(RULES, NUMCPUS)
